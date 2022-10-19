a17b5c95f1bee190944656197fba16c6bf07c25e
masyaka.94 <masyaka.94@gmail.com>
2022-10-19 19:46:27 +0500

    Zadacha 36

---
+++ Zadacha36/Program.cs
@@ -0,0 +1,33 @@
+﻿/*36. Задайте одномерный массив заполненный случайными числами. 
+Найдите сумму элементов, стоящих на нечётных позициях*/
+
+void FillArray(int[] massive)
+{
+int length = massive.Length;
+
+for (int index = 0; index<length; index++)
+{
+massive[index]=new Random().Next(100,1000);
+}
+}
+
+void PrintArray(int[] mass)
+{
+foreach (int position in mass)
+{
+Console.Write($"{position} ");
+}
+}
+
+Console.Write($"Введите размер массива:  ");
+int x = int.Parse(Console.ReadLine()!);
+int[] array = new int [x];
+FillArray(array);
+PrintArray(array);
+int sum = 0;
+
+ for (int i = 0; i<array.Length; i++)
+ 	{
+	if (i %2 != 0) sum = sum + array [i];
+	}
+Console.Write($"Сумма чисел, стоящих на нечётных позициях равна {sum}");
